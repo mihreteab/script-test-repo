8d1fd60627d4785bdaf6258ca62d4b8e816f0feb
Mihreteab <mihreteabdemeke@gmail.com>
2021-11-17 19:38:57 +0300

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    //console.log("it is working");
+
+
+export default greeting(){
+    document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("it is working");
     var h1 = document.createElement('h1');
     h1.appendChild(document.createTextNode("Welcome"));
     document.body.appendChild(h1)
-});
+    });
+}
